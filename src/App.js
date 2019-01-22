af52e5031b511a2147cd83c8cb6f226b5058e4d9
Suraj Sharma <www.surajsharma@gmail.com>
2019-01-22 07:39:26 +0530

    HeaderNav working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HeaderNav from './containers/HeaderNav/HeaderNav'
 
 class App extends Component {
   render() {
     return (
-        <div>Hello World</div>
+        <HeaderNav/>
     );
   }
 }
