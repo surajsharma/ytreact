44ad036a76e0d0a1f46d2a8351c1f945c364367c
Suraj Sharma <www.surajsharma@gmail.com>
2019-02-02 12:40:06 +0530

    sidebar fixed

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import HeaderNav from './containers/HeaderNav/HeaderNav'
-
+import SideBar from './containers/SideBar/SideBar'
 class App extends Component {
   render() {
     return (
-        <HeaderNav/>
+        <div>
+            <HeaderNav/>
+            <SideBar/>
+        </div>
     );
   }
 }
