736e1e6f4a9f07cd167f824cb336d0f44632156e
Suraj Sharma <www.surajsharma@gmail.com>
2019-02-11 11:56:01 +0530

    subscriptions in sidebar

--- src/containers/SideBar/SideBar.js
+++ src/containers/SideBar/SideBar.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Menu, Divider } from "semantic-ui-react";
 import { SideBarItem } from "./SideBarItem/SideBarItem";
+import {SideBarHeader} from './SideBarHeader/SideBarHeader';
+import {Subscriptions} from './Subscriptions';
 import "./SideBar.scss";
-import {SideBarHeader} from './SideBarHeader/SideBarHeader'
 
 export class SideBar extends React.Component {
     render() {
@@ -17,14 +18,20 @@ export class SideBar extends React.Component {
                 <SideBarItem highlight={true}  label='Home' icon='home'/>
                 <SideBarItem label='Trending' icon='fire'/>
                 <SideBarItem label='Followers' icon='spy'/>
+
                 <Divider/>
+
                 <SideBarHeader title='Library'/>
                 <SideBarItem label='History' icon='history'/>
                 <SideBarItem label='Watch later' icon='clock'/>
                 <SideBarItem label='Liked videos' icon='thumbs up'/>
                 <Divider/>
+
+                <Subscriptions />
+
                 <SideBarItem label='Movies and Shows' icon='film'/>
                 <Divider/>
+
                 <SideBarItem label='Report history' icon='flag'/>
                 <SideBarItem label='Help' icon='help circle'/>
                 <SideBarItem label='Send feedback' icon='comment'/>
---
+++ src/containers/SideBar/Subscriptions.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Subscription} from './Subscription/Subscription';
+import {Divider} from 'semantic-ui-react';
+import {SideBarHeader} from './SideBarHeader/SideBarHeader';
+
+
+export class Subscriptions extends React.Component {
+    render() {
+        return (
+                <React.Fragment>
+                    <SideBarHeader title='Subscriptions' />
+                    <Subscription label='MusicChannel' broadcasting />
+                    <Subscription label='Coursera' amountNewVideos={10} />
+                    <Subscription label='TEDx'  amountNewVideos={23} />
+                    <Subscription label='Standford iOS'  amountNewVideos={4} />
+                    <Subscription label='Udacity'  amountNewVideos={114} />
+                    <Divider/>
+                </React.Fragment>
+        )
+    }
+}
