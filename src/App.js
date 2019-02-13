ff97bb54c7980443e00f2c90c0878ec43a0e6e50
Suraj Sharma <www.surajsharma@gmail.com>
2019-02-13 07:43:47 +0530

    videopreview

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import HeaderNav from './containers/HeaderNav/HeaderNav'
 import SideBar from './containers/SideBar/SideBar'
+import {Home} from './containers/Home/Home'
 class App extends Component {
   render() {
     return (
         <div>
             <HeaderNav/>
             <SideBar/>
+            <Home/>
         </div>
     );
   }
